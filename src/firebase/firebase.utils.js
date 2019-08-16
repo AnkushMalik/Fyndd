ff3ae39dd9efd16c85ece540f064929dee88ff14
Ankush Malik <ankushmalik631@gmail.com>
2019-08-17 00:44:14 +0530

    Add firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAdKxn5LYoGHMdd4Xks61J9bBwqDwDrZJg",
+    authDomain: "fynddd.firebaseapp.com",
+    databaseURL: "https://fynddd.firebaseio.com",
+    projectId: "fynddd",
+    storageBucket: "",
+    messagingSenderId: "231976529891",
+    appId: "1:231976529891:web:07b6642d8e118abf"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
