1ca1073cb7b46ded727e6507d6c57b955cc14009
Ankush Malik <ankushmalik631@gmail.com>
2019-08-16 20:01:07 +0530

    Add SignIn component

--- src/components/accounts/signin.component.jsx
+++ src/components/accounts/signin.component.jsx
@@ -4,13 +4,41 @@ class SignIn extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            email: '',
+            password: ''
         }
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ email: '', password: '' })
+    }
+    handleChange = e => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value })
     }
     render() {
         return (
-            < div className="signin-section" >
-                <div>Sign In</div>
+            < div className="sign-in" >
+                <h2>I already have an account</h2>
+                <span>Sign In with your registered email</span>
+                <form onSubmit={this.handleSubmit}>
+                    <h5>Email</h5>
+                    <input
+                        name='email'
+                        type='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <h5>Password</h5>
+                    <input
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <input type='submit' value='submit' />
+
+                </form>
             </div >
         )
     }
