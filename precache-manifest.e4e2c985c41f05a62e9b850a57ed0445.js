b76d8f58f474978954cc958b3ad8e16b51194846
AnkushMalik <ankushmalik631@gmail.com>
2020-07-20 11:14:30 +0530

    Updates

---
+++ precache-manifest.e4e2c985c41f05a62e9b850a57ed0445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665bcc162eaeed3d0f5c62218afe1647",
+    "url": "/Fyndd/index.html"
+  },
+  {
+    "revision": "12519c37f0fb56ed585a",
+    "url": "/Fyndd/static/css/main.d8b9bf16.chunk.css"
+  },
+  {
+    "revision": "b37990f8b42d7b66b7b9",
+    "url": "/Fyndd/static/js/2.962a8a10.chunk.js"
+  },
+  {
+    "revision": "12519c37f0fb56ed585a",
+    "url": "/Fyndd/static/js/main.718f1182.chunk.js"
+  },
+  {
+    "revision": "3e96e006c09db4d78c27",
+    "url": "/Fyndd/static/js/runtime~main.5b1d614b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f48a3a219123c26674191f32f0009f60.js"
+  "/Fyndd/precache-manifest.e4e2c985c41f05a62e9b850a57ed0445.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fyndd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
